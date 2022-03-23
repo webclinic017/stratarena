beb9880c6fb0bf25ea9061e1181c60c14a21dd6d
exopyl <88846154+exopyl@users.noreply.github.com>
2022-03-23 14:07:00 +0100

    :rocket:

--- main.py
+++ main.py
@@ -1,2 +1,48 @@
-if __name__ == "__main__":
-	print("stratarena...")
+from strategies import RandomStrategy
+import backtrader as bt
+
+
+# Create a Stratey
+class TestStrategy(bt.Strategy):
+
+    def log(self, txt, dt=None):
+        ''' Logging function for this strategy'''
+        dt = dt or self.datas[0].datetime.date(0)
+        print('%s, %s' % (dt.isoformat(), txt))
+
+    def __init__(self):
+        # Keep a reference to the "close" line in the data[0] dataseries
+        self.dataclose = self.datas[0].close
+
+    def next(self):
+        # Simply log the closing price of the series from the reference
+        self.log('Close, %.2f' % self.dataclose[0])
+
+
+if __name__ == '__main__':
+    # Create a cerebro entity
+    cerebro = bt.Cerebro()
+
+    # Add a strategy
+    cerebro.addstrategy(RandomStrategy.RandomStrategy)
+
+    # Create a Data Feed
+    data = bt.feeds.YahooFinanceCSVData(dataname='./ACA.PA.csv')
+
+    # Add the Data Feed to Cerebro
+    cerebro.adddata(data)
+
+    # Set our desired cash start
+    cerebro.broker.setcash(100000.0)
+
+    # Print out the starting conditions
+    print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+    # Run over everything
+    cerebro.run()
+
+    # Print out the final result
+    print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+    figure = cerebro.plot()[0][0]
+    figure.savefig('example.png')
