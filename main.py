8033f2bcd7b073dc77d840ed4cb645b2aeb0dbfa
exopyl <88846154+exopyl@users.noreply.github.com>
2022-03-23 14:50:47 +0100

    clean

--- main.py
+++ main.py
@@ -1,24 +1,6 @@
 from strategies import RandomStrategy
 import backtrader as bt
 
-
-# Create a Stratey
-class TestStrategy(bt.Strategy):
-
-    def log(self, txt, dt=None):
-        ''' Logging function for this strategy'''
-        dt = dt or self.datas[0].datetime.date(0)
-        print('%s, %s' % (dt.isoformat(), txt))
-
-    def __init__(self):
-        # Keep a reference to the "close" line in the data[0] dataseries
-        self.dataclose = self.datas[0].close
-
-    def next(self):
-        # Simply log the closing price of the series from the reference
-        self.log('Close, %.2f' % self.dataclose[0])
-
-
 if __name__ == '__main__':
     # Create a cerebro entity
     cerebro = bt.Cerebro()
